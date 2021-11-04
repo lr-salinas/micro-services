4a9ccd4ad06c78bdf710d9f9e6b531a2e1d698ff
lr-salinas <laura.salinas.r@ucb.edu.bo>
2021-11-04 10:15:12 -0400

    adding ms-discovery

---
+++ ms-discovery/ms-discovery/src/test/java/arquitecturasoftware/msdiscovery/MsDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package arquitecturasoftware.msdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
